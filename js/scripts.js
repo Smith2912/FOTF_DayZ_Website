5416646a88fcd4b0f9d9ab5b427840e230f7d1bf
Smith2912 <84427608+Smith2912@users.noreply.github.com>
2024-12-29 21:42:31 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     var audio = document.getElementById('background-audio');
     audio.volume = 0.1;
     audio.muted = false;
-});
 
-// Slideshow script
-let slideIndex = 0;
-showSlides();
+    let slideIndex = 0;
+    showSlides();
 
-function showSlides() {
-    let slides = document.getElementsByClassName("slideshow-image");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("active");
+    function showSlides() {
+        let slides = document.getElementsByClassName("slideshow-image");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("active");
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].classList.add("active");
+        setTimeout(showSlides, 5000); // Change image every 5 seconds
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].classList.add("active");
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
+});
 
 function resizeBattleMetrics() {
     const wrapper = document.querySelector('.battlemetrics-wrapper');
